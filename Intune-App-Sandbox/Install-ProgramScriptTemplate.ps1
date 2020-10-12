b805c9727b4b57d7ad7c52368238e4300a6752a8
UniverseCitiz3n <maciej.horbacz@gmail.com>
2020-10-12 21:54:36 +0200

    template

--- Intune-App-Sandbox/Install-ProgramScriptTemplate.ps1
+++ Intune-App-Sandbox/Install-ProgramScriptTemplate.ps1
@@ -1,6 +1,5 @@
-﻿$global:ErrorActionPreference = "Stop"
-# Parameters
-$FileName = "VSCodeUserSetup-x64-1.47.0.exe"
+﻿# Parameters
+$FileName = "VSCodeSetup-x64-1.50.0.exe"
 $Tag = 'VSCode'
 $OperatingFolder = 'C:\Program Files (x86)\Microsoft\Temp'
 $ArgumentListInstallation = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /NOCANCEL /SP- /LOG="{0}\{1}Install.log" /MERGETASKS=!runcode' -f $OperatingFolder, $Tag
@@ -27,28 +26,30 @@ function Exit-WithCode {
 
 #Check for folder
 If (Test-Path -Path $OperatingFolder -PathType Container) {
-	Write-FileLog -FunctionStart -LogFile $LogFile -LogOverWrite
+	Write-FileLog -FunctionStart -LogFile $LogFile
 } Else {
-	Write-FileLog -FunctionStart -LogFile $LogFile -LogOverWrite
 	New-Item -Path $OperatingFolder -ItemType Directory
-	Write-FileLog -Message "$OperatingFolder created"  -LogFile $LogFile -LogOverWrite
+	Write-FileLog -Message "$OperatingFolder created" -LogFile $LogFile
 }
 
 #Install
-Write-FileLog -Message "Installation with arguments: $ArgumentListInstallation"  -LogFile $LogFile -LogOverWrite
+Write-FileLog -FunctionStart -LogFile $LogFile
+Write-FileLog -Message "Installation with arguments: $ArgumentListInstallation" -LogFile $LogFile
 Try {
-	$Process = Start-Process $PSScriptRoot\$FileName -ArgumentList $ArgumentListInstallation -NoNewWindow -PassThru -Wait
-	Write-FileLog -Message "Installation exit code: $($Process.ExitCode)"  -LogFile $LogFile -LogOverWrite
+	$Process = Start-Process $PSScriptRoot\$FileName -ArgumentList $ArgumentListInstallation -NoNewWindow -PassThru -Wait -ErrorAction Stop
+	Write-FileLog -Message "Installation exit code: $($Process.ExitCode)" -LogFile $LogFile
 
 	If ($Process.ExitCode -ne 0) {
-		Write-FileLog -Type e -Message "Installation failed. Please check $OperatingFolder\${Tag}Install.log" -LogFileError $LogFileError -LogOverWrite
+		Write-FileLog -Type Error -Message "Installation failed. Please check $OperatingFolder\${Tag}Install.log" -LogFileError $LogFileError
 		Exit-WithCode -exitcode $InstallFailCode
 	} else {
-		Write-FileLog -Type i -Message "Installation $FileName SUCCESS"
+		Write-FileLog -Message "Installation $FileName SUCCESS" -LogFile $LogFile
 		Exit-WithCode -exitcode $InstallSuccessCode
 	}
 } Catch {
-	Write-FileLog -Type e -Message "Script ERROR" -LogFileError $LogFileError -LogOverWrite
+	Write-FileLog -Type Error -Message "Script ERROR" -LogFileError $LogFileError
 	$_ | Out-File -FilePath $LogFileError -Append -Encoding ASCII
+	Write-FileLog -Type Warn -Message "Script TERMINATION" -LogFileError $LogFileError
+	Write-FileLog -Type Warn -Message "Exitcode $InstallFailCode" -LogFileError $LogFileError
 	Exit-WithCode -exitcode $InstallFailCode
 }

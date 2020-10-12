51a362a126ec7b731ac286136251ad2821d92f53
UniverseCitiz3n <maciej.horbacz@gmail.com>
2020-10-12 20:29:15 +0200

    script template, write file log function

---
+++ Intune-App-Sandbox/Install-ProgramScriptTemplate.ps1
@@ -0,0 +1,54 @@
+﻿$global:ErrorActionPreference = "Stop"
+# Parameters
+$FileName = "VSCodeSetup-x64-1.46.1.exe"
+$Tag = 'VSCode'
+$OperatingFolder = 'C:\Program Files (x86)\Microsoft\Temp'
+$ArgumentListInstallation = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /NOCANCEL /SP- /LOG="{0}\{1}Install.log" /MERGETASKS=!runcode' -f $OperatingFolder, $Tag
+$InstallFailCode = 1707
+$InstallSuccessCode = 1641
+
+# Log
+$LogFile = "$OperatingFolder\IntuneSoftwareInstall.log"
+$LogFileError = "$OperatingFolder\IntuneSoftwareInstallError.log"
+
+#Info
+. $PSScriptRoot\Write-FileLog.ps1
+
+#Custom exit
+function Exit-WithCode {
+	param
+	(
+		$exitcode
+	)
+
+	$host.SetShouldExit($exitcode)
+	exit
+}
+
+#Check for folder
+If (Test-Path -Path $OperatingFolder -PathType Container) {
+	Write-FileLog -FunctionStart -LogFile $LogFile -LogOverWrite
+} Else {
+	Write-FileLog -FunctionStart -LogFile $LogFile -LogOverWrite
+	New-Item -Path $OperatingFolder -ItemType Directory
+	Write-FileLog -Message "$OperatingFolder created"  -LogFile $LogFile -LogOverWrite
+}
+
+#Install
+Write-FileLog -Message "Installation with arguments: $ArgumentListInstallation"  -LogFile $LogFile -LogOverWrite
+Try {
+	$Process = Start-Process $PSScriptRoot\$FileName -ArgumentList $ArgumentListInstallation -NoNewWindow -PassThru -Wait
+	Write-FileLog -Message "Installation exit code: $($Process.ExitCode)"  -LogFile $LogFile -LogOverWrite
+
+	If ($Process.ExitCode -ne 0) {
+		Write-FileLog -Type e -Message "Installation failed. Please check $OperatingFolder\${Tag}Install.log" -LogFileError $LogFileError -LogOverWrite
+		Exit-WithCode -exitcode $InstallFailCode
+	} else {
+		Write-FileLog -Type i -Message "Installation $FileName SUCCESS"
+		Exit-WithCode -exitcode $InstallSuccessCode
+	}
+} Catch {
+	Write-FileLog -Type e -Message "Script ERROR" -LogFileError $LogFileError -LogOverWrite
+	$_ | Out-File -FilePath $LogFileError -Append -Encoding ASCII
+	Exit-WithCode -exitcode $InstallFailCode
+}
---
+++ Intune-App-Sandbox/Write-FileLog.ps1
@@ -0,0 +1,88 @@
+﻿Function Write-FileLog {
+	[CmdletBinding()]
+	Param (
+		# Type can be: 'i' for Info, 'w' for Warn, 'e' for Error)]
+		[Alias('t')]
+		[Parameter()]
+		[ValidateSet('i', 'w', 'e')]
+		[System.String]$Type = 'i',
+		# Message
+		[Alias('m')]
+		[Parameter()]
+		[System.String]$Message,
+		# Message
+		[Alias('s')]
+		[Parameter()]
+		[Switch]$FunctionStart,
+		# ProcessName will mark in log which function is running now
+		[Alias('lf')]
+		[Parameter()]
+		[System.String]$LogFile,
+		# ProcessName will mark in log which function is running now
+		[Alias('lfe')]
+		[Parameter()]
+		[System.String]$LogFileError,
+		# Message
+		[Alias('o')]
+		[Parameter()]
+		[Switch]$LogOverWrite
+	)
+	if ($LogOverWrite) {
+		$Append = $false
+	} else {
+		$Append = $true
+	}
+
+	if ($Type -eq 'e') {
+		$FilePath = $LogError
+	} else {
+		$FilePath = $Log
+	}
+
+	if ($Type -eq 'i') {
+		$Type = 'Info'
+	} elseif ($Type -eq 'w') {
+		$Type = 'Warn'
+	} elseif ($Type -eq 'e') {
+		$Type = 'Error'
+	} else {
+		$Type = 'Info'
+	}
+
+	if ($FunctionStart -and -not $Message) {
+		$Message = 'START function'
+	}
+
+	$callerFunction = (Get-PSCallStack)[1]
+	if ($callerFunction.InvocationInfo -and $callerFunction.InvocationInfo.MyCommand -and $callerFunction.InvocationInfo.MyCommand.Name) {
+		$callerNameCommand = $callerFunction.InvocationInfo.MyCommand.Name
+	} else {
+		$callerNameCommand = '';
+	}
+	if ($callerFunction.ScriptName) {
+		$callerNameScript = Split-Path -Leaf $callerFunction.ScriptName
+	} else {
+		$callerNameScript = '';
+	}
+	if ($callerFunction.ScriptLineNumber) {
+		$callerLineNumber = $callerFunction.ScriptLineNumber
+	} else {
+		$callerLineNumber = ''
+	}
+
+	if ($callerNameCommand -eq '' -and $callerNameScript -ne '') {
+		$callerNameCommand = $callerNameScript
+	}
+
+	$timestamp = Get-Date -Format 'yyyy-MM-dd HH:mm:ss'
+
+	$msg = "[$timestamp] [$callerNameCommand] [$callerLineNumber] [$Type] $Message"
+
+	$msg | Out-File -FilePath $FilePath -Append:$Append -Encoding ASCII
+
+	Write-Verbose -Message $msg
+
+	if ($Type -eq 'Error') {
+		Throw $msg
+	}
+}

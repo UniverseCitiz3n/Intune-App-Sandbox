b82b7dbf4007ea2a1644667da69b4b7d572c5f1e
UniverseCitiz3n <maciej.horbacz@gmail.com>
2021-01-18 21:31:26 +0100

    :D

--- tests/Unit.Tests.ps1
+++ tests/Unit.Tests.ps1
@@ -1,11 +1,11 @@
 Describe "CHECK CONFIGURATION FILES ARE PRESENT" {
 	It "Check configuration files are present" {
-		(Get-ChildItem /tests/ -Recurse | Measure-Object).Count | Should Be 8
+		(Get-ChildItem .\Configuration -Recurse | Measure-Object).Count | Should Be 8
 	}
 	$Files = ('intunewin-Box-icon.ico', 'IntuneWinAppUtil.exe', 'IntuneWinAppUtilDecoder.exe', 'Invoke-IntunewinUtil.ps1', 'Invoke-Test.ps1', 'New-ToastNotification.ps1', 'sandbox.ico', 'toast.xml')
 	foreach ($item in $Files) {
 		It "Checking for $item" {
-			Test-Path /tests/$item | Should Be $true
+			Test-Path .\Configuration\$item | Should Be $true
 		}
 	}
 }

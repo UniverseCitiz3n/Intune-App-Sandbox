05ab2e5b16c0a476c22cbe7fb9a5df36d10ea00d
UniverseCitiz3n <maciej.horbacz@gmail.com>
2021-07-08 16:17:02 +0200

    ✨
    find device installed soft and in user context

--- Intune-App-Sandbox/Helpers/Get-WindowsInstalledPrograms.ps1
+++ Intune-App-Sandbox/Helpers/Get-WindowsInstalledPrograms.ps1
@@ -1,4 +1,8 @@
-﻿Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*',
+﻿New-PSDrive HKU Registry HKEY_USERS
+Get-ItemProperty -Path 'HKU:\*\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*',
+'HKU:\*\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+'HKU:\*\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*',
+'HKU:\*\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*', 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*',
 'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*',
 'HKCU:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' -ErrorAction Ignore |

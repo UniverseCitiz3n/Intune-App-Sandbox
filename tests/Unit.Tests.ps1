4e88e098dbc0b149bf6b380a8446e095b6279a00
UniverseCitiz3n <maciej.horbacz@gmail.com>
2021-09-29 15:44:41 +0200

    🩹 ✅ another test fix

--- tests/Unit.Tests.ps1
+++ tests/Unit.Tests.ps1
@@ -2,7 +2,7 @@ Describe "CHECK CONFIGURATION FILES ARE PRESENT" {
 	It "Check configuration files are present" {
 		(Get-ChildItem .\Intune-App-Sandbox\Configuration\* -Recurse | Measure-Object).Count | Should -Be 9
 	}
-	$Files = ('intunewin-Box-icon.ico', 'IntuneWinAppUtil.exe', 'IntuneWinAppUtilDecoder.exe', 'Invoke-IntunewinUtil.ps1', 'Invoke-Test.ps1', 'New-ToastNotification.ps1', 'sandbox.ico', 'toast.xml')
+	$Files = ('intunewin-Box-icon.ico', 'IntuneWinAppUtil.exe', 'IntuneWinAppUtilDecoder.exe', 'Invoke-IntunewinUtil.ps1', 'Invoke-Test.ps1', 'New-ToastNotification.ps1', 'sandbox.ico', 'sandbox_detection.ico', 'toast.xml')
 	foreach ($item in $Files) {
 		It "Checking for $item" {
 			Test-Path .\Intune-App-Sandbox\Configuration\$item | Should -Be $true

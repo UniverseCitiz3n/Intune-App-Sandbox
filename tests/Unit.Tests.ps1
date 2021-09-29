cfa454f0bc63e1f18605653096b1f235f531ee44
UniverseCitiz3n <maciej.horbacz@gmail.com>
2021-09-29 15:43:45 +0200

    ✅ pester test update

--- tests/Unit.Tests.ps1
+++ tests/Unit.Tests.ps1
@@ -1,6 +1,6 @@
 Describe "CHECK CONFIGURATION FILES ARE PRESENT" {
 	It "Check configuration files are present" {
-		(Get-ChildItem .\Intune-App-Sandbox\Configuration\* -Recurse | Measure-Object).Count | Should -Be 8
+		(Get-ChildItem .\Intune-App-Sandbox\Configuration\* -Recurse | Measure-Object).Count | Should -Be 9
 	}
 	$Files = ('intunewin-Box-icon.ico', 'IntuneWinAppUtil.exe', 'IntuneWinAppUtilDecoder.exe', 'Invoke-IntunewinUtil.ps1', 'Invoke-Test.ps1', 'New-ToastNotification.ps1', 'sandbox.ico', 'toast.xml')
 	foreach ($item in $Files) {
